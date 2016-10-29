f4b1ba4dc4525cd80a343e011794b50fddfc6c81
Josh Atwell <atwellj93@gmail.com>
2016-10-29 04:08:28 +0000

    heroku seems to be fixed :) woot woot

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,17 +6,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
-  # storage :file
+ #storage :file
+  storage :fog
   
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,8 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['aws_access_key_id'],
-      :aws_secret_access_key => ENV['aws_secret_access_key'],
-    }
-    config.fog_directory = ENV['fog_directory']
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
