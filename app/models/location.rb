51faa4a26464324e9a850c6c5ff73a12813e9fdb
Josh Atwell <atwellj93@gmail.com>
2016-11-08 08:12:54 +0000

    moving on for now'

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,6 +7,6 @@ class Location < ActiveRecord::Base
   has_many :reviews 
   
   validates :name, :address, :image, presence: true
-  validates :address, format: { with: /\A\d+[^,]+,[^,]+ [A-Z]{2,4} \d{2,6}\z/,
+  validates :address, format: { with: /\A\S+[^,]+,[^,]+[a-zA-Z]{2,10} \d{2,6}\z/,
     message: "must be in the format; 123 Fake St, Melbourne VIC 3056 (Number + Street name + Type i.e St, Rd, Cr, Dr etc.), (Town or city + State QLD, NSW, NT etc. + Postcode" }
 end
