f0e871d901bee2fab69f38501715cc59c9c903d9
Josh Atwell <atwellj93@gmail.com>
2016-12-04 07:43:03 +0000

    remove sitemap

--- app/models/sitemap.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'sitemap_generator'
-
-SitemapGenerator::Sitemap.default_host = 'https://rutbustin.herokuapp.com/'
-SitemapGenerator::Sitemap.create do
-  add '/locations', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
-end
-SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
