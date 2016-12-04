d438f7fb3f5cbce925d428a8e4d828a5d449b31d
Josh Atwell <atwellj93@gmail.com>
2016-12-04 07:36:11 +0000

    site map

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -3,7 +3,7 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://rutbustin.herokuapp.com/'
 SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'daily', :priority => 0.9
+  add '/locations', :changefreq => 'daily', :priority => 0.9
   add '/contact_us', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
