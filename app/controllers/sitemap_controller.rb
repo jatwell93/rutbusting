df9c0c02fa64489fdd4ba55c57e71298bed59e72
Josh Atwell <atwellj93@gmail.com>
2016-12-09 03:17:25 +0000

    sitemap fix

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,11 +1,11 @@
 # app/controllers/sitemap_controller.rb
- 
+
 SitemapController < ApplicationController
   layout nil
   def index
     headers['Content-Type'] = 'application/xml'
     respond_to do |format|
-      format.xml {@posts = Post.all}
+      format.xml {@locaitons = Location.all}
     end
   end
 end
